e4e347c8fd1a33bfcaba2cdda27cb75b4df931e1
kmahmud <kmahmud@mahmudhp>
2020-12-14 17:21:21 -0500

    added another class.java

---
+++ shahin/src/main/java/shahin1/Test1.java
@@ -0,0 +1,10 @@
+package shahin1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("first test");
+
+	}
+
+}
---
+++ shahin/src/main/java/shahin1/shahin2.java
@@ -0,0 +1,10 @@
+package shahin1;
+
+public class shahin2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git account Test");
+	}
+
+}
